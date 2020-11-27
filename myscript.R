205338b89ec0e5b41f51b4f15bfc394e5abf0566
Callum Bucklow <callumbucklow@Callums-MacBook-Pro.local>
2020-11-27 15:21:04 +0000

    hh

--- myscript.R
+++ myscript.R
@@ -12,3 +12,4 @@ fit = nls(ydata ~ p1*cos(p2*xdata) + p2*sin(p1*xdata), data = mydata,
 #Plot the fitted line
 new = data.frame(xdata = seq(min(mydata$xdata),max(mydata$xdata),len=200))
 lines(new$xdata,predict(fit,newdata=new))
+
